b8ff1cf3a8c9af347c2cdcb199b0d6d0eb8fc7f6
jesseleadt <jesseleadt@gmail.com>
2023-12-15 20:10:14 -0500

    Updates

---
+++ precache-manifest.3e32c5997129747b8096617396ab5e61.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "30933cff95255698b808",
+    "url": "/static/js/0.00ba5458.chunk.js"
+  },
+  {
+    "revision": "db8b7d7000ee4a9e2d6b",
+    "url": "/static/js/1.6aa85564.chunk.js"
+  },
+  {
+    "revision": "07001687102c0aa92fe1",
+    "url": "/static/js/main.260e7da5.chunk.js"
+  },
+  {
+    "revision": "1a40e7b931d7189afbf1",
+    "url": "/static/js/runtime~main.c69e80f1.js"
+  },
+  {
+    "revision": "1d6e03274554b2ae5362",
+    "url": "/static/js/4.feed2e1c.chunk.js"
+  },
+  {
+    "revision": "a2feace29283c3649e64",
+    "url": "/static/js/5.cd3f3c1d.chunk.js"
+  },
+  {
+    "revision": "5327e33f1e3fd23ff43b",
+    "url": "/static/css/6.4b6ed111.chunk.css"
+  },
+  {
+    "revision": "5327e33f1e3fd23ff43b",
+    "url": "/static/js/6.f48a731e.chunk.js"
+  },
+  {
+    "revision": "5f5f581e03d6429f6d48",
+    "url": "/static/js/7.ccfec9c2.chunk.js"
+  },
+  {
+    "revision": "12058796f58ee37a1fd6",
+    "url": "/static/css/8.f68ab53d.chunk.css"
+  },
+  {
+    "revision": "12058796f58ee37a1fd6",
+    "url": "/static/js/8.be1aab2b.chunk.js"
+  },
+  {
+    "revision": "32b1555696cc8c2009dc",
+    "url": "/static/js/9.7b8a3cac.chunk.js"
+  },
+  {
+    "revision": "40134204e13e74d2497a",
+    "url": "/static/js/10.1c951ca5.chunk.js"
+  },
+  {
+    "revision": "2974fea89f027f8a7c12",
+    "url": "/static/js/11.09909fc5.chunk.js"
+  },
+  {
+    "revision": "1aa307cd477e430718ae",
+    "url": "/static/js/12.562a1b50.chunk.js"
+  },
+  {
+    "revision": "61bf04cde1a397d1587d",
+    "url": "/static/js/13.7a070fbe.chunk.js"
+  },
+  {
+    "revision": "902ad2e6608a7ae5a9d3",
+    "url": "/static/js/14.9b198f07.chunk.js"
+  },
+  {
+    "revision": "2d151202649ccca1c728",
+    "url": "/static/js/15.3264ea68.chunk.js"
+  },
+  {
+    "revision": "51ed2eb3486401879fe5",
+    "url": "/static/js/16.c296a90b.chunk.js"
+  },
+  {
+    "revision": "5531bb1802d8cfa641d0",
+    "url": "/static/js/17.9cf2c838.chunk.js"
+  },
+  {
+    "revision": "9717dd55443dc34fb030",
+    "url": "/static/js/18.81c4b5f6.chunk.js"
+  },
+  {
+    "revision": "ced24bee3f474b1f6a5d",
+    "url": "/static/js/19.5355dc2e.chunk.js"
+  },
+  {
+    "revision": "fc906e30fc92d4a368be",
+    "url": "/static/js/20.cd858d73.chunk.js"
+  },
+  {
+    "revision": "3b6e26041234efc23e7268a9d9fe9938",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ngb-app/precache-manifest.36f217e029607518bcacd0e6b29e060c.js"
+  "/precache-manifest.3e32c5997129747b8096617396ab5e61.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ngb-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
